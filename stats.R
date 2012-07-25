29338cf7e4d2cc0d5f99b8d914efbc228f6fca66
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-07-25 12:15:02 -0700

    all the plots

--- stats.R
+++ stats.R
@@ -26,26 +26,38 @@ for(thread in threads){
 b$LLC_MISS_RATIO = b$LLC_MISSES/b$LLC_REFERENCES
 b$PAPI_L2_DCM_RATIO = b$PAPI_L2_DCM/b$PAPI_L2_DCA
 b$PERF_COUNT_HW_CACHE_L1D_MISS_RATIO = b$'perf::PERF_COUNT_HW_CACHE_L1D:MISS' / b$'perf::PERF_COUNT_HW_CACHE_L1D:ACCESS'
+b$stall_ratio = b$PAPI_STL_ICY / b$TOT_CYC
 
-dev.new()
-first = T
-ylim=c(0,max(b$mean_time))
-colors = rainbow(length(accesses))
-index = 0
+for(name in names(b)){
 
-for(access in accesses){
-  index = index + 1
-  sel = b$access == access
-  if(first){
-    plot(b$mean_time[sel] ~ b$threads[sel], 
-         main='run time vs # of threads',
-         ylim=ylim,
-	 col = colors[index]
-         )
-    first = F
-  } else {
-    points(b$mean_time[sel] ~ b$threads[sel],
+  if(class(b[[name]]) != "numeric") next
+
+  dev.new()
+  first = T
+  ylim=c(0,max(b[[name]]))
+  colors = rainbow(length(accesses))
+  index = 0
+  
+  for(access in accesses){
+    index = index + 1
+    sel = b$access == access
+    if(first){
+      plot(b[[name]][sel] ~ b$threads[sel], 
+           main=paste(name, 'vs # of threads'),
+           ylim=ylim,
+	   ylab=name,
+	   xlab='threads',
            col = colors[index]
-          )
+           )
+      first = F
+    } else {
+      points(b[[name]][sel] ~ b$threads[sel],
+             col = colors[index]
+            )
+    }
   }
+  legend(x='topright', 
+         legend=accesses,
+	 col=colors,
+	 lwd=3)
 }
